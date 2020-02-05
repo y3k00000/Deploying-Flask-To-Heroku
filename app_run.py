c6b126e8ac9ada43d9d4ce967dc7dc065b48559d
y3k <y3k00000@gmail.com>
2020-02-05 22:07:48 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -1,11 +1,12 @@
 from flask import *
+from pypinyin import pinyin, lazy_pinyin, Style
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello World!!"
+@app.route('/<path:any>')
+def index(any):
+    str = request.args.get("str")
+    return "Hello World!! "+str
 
 
 if __name__ == '__main__':
