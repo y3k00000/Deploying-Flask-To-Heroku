ca95473d4a223486fde5682ea7bc482fa62edab5
y3k <y3k00000@gmail.com>
2020-02-05 22:20:15 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -1,11 +1,13 @@
 from flask import *
 from pypinyin import pinyin, lazy_pinyin, Style
+import urllib.parse
 
 app = Flask(__name__)
 
 @app.route('/pinyin')
 def index():
     str = request.args.get("str")
+    str = urllib.parse.unquote(str)
     return "result = "+pinyin(str,style=Style.BOPOMOFO)
 
 
