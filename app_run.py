7513ec89c1b37a1d6771da844381d25cc104633a
y3k <y3k00000@gmail.com>
2020-02-06 10:56:51 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -1,6 +1,7 @@
 from flask import *
 from pypinyin import pinyin, lazy_pinyin, Style
 import urllib.parse
+import json
 
 app = Flask(__name__)
 
@@ -9,8 +10,9 @@ def index():
     strToConvert = request.args.get("str")
     strToConvert = urllib.parse.unquote_plus(strToConvert)
     converted = pinyin(strToConvert,style=Style.BOPOMOFO)
-    converted = [item[0] for item in converted]
-    converted = " ".join(converted)
+    converted = json.dump(converted)
+#     converted = [item[0] for item in converted]
+#     converted = " ".join(converted)
     return converted
 
 if __name__ == '__main__':
